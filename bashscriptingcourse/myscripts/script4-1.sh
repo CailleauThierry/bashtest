8eb72126a795251417ad8ffb80169a8551c4347f
CailleauThierry <thierrycailleay@yahoo.fr>
2020-02-10 09:45:17 +0000

    fix to script4-1.sh for ubuntu

--- bashscriptingcourse/myscripts/script4-1.sh
+++ bashscriptingcourse/myscripts/script4-1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #!/bin/bash
-if [ test -a FILE /etc/shadow ]
+if [ test -f FILEORFOLDER /etc/shadow ]
   then
-    echo "Shadow passwords are enabled."
-    if [ -w FILE /etc/shadow ]
+    echo "This is not a File"
+    if [ -d FILEORFOLDER /etc/shadow ]
       then
-        echo "You have permissions to edit /etc/shadow."
+        echo "This is not a folder"
       else
-        echo "You do not have permissions to edit /etc/shadow."
+        echo "This is a folder"
     fi
-    else
-      echo "Shadow passwords are disabled."
+  else
+      echo "This is a File"
 fi
 # script4-1.sh on 02_01_2020 by Thierry Cailleau following a https://www.udemy.com/course/bash-scripting
 # Tested on Ubuntu 19.10
